9a8e4e3c1c9cece7c6fbd928487e3db38c68c7ea
Tina <tina.reymann@gmail.com>
2015-02-18 12:35:24 +1100

    add mobile menu barebones

--- js/script.js
+++ js/script.js
@@ -7,6 +7,12 @@
 	    $(this).parent().fadeOut('slow');
 	});	
 
+	// Mobile Menu
+	$('.menu-open, .menu-close').click(function(e) {
+		e.preventDefault();
+		$('header .navigation').toggleClass('open');
+	});
+
 	// Clear input field value on focus and restore if no value was entered
 	$('input[type="text"],input[type="email"]').focus(function() {
 		if( this.value == this.defaultValue ) {
--- templates/inc/header.php
+++ templates/inc/header.php
@@ -2,7 +2,7 @@
   <div class="header-wrapper">
   <header role="banner" class="row" id="top">
 
-    <div class="branding medium-4 small-12 columns"> 
+    <div class="branding medium-4 columns"> 
         <a href="<?php print $front_page; ?>" title="Return to homepage" rel="home">
           <!-- <img alt="<?php print t('Home'); ?>" class="site-logo" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" />-->
           <?php if ($site_name || $site_slogan): ?>
@@ -21,14 +21,19 @@
         <?php print render($page['global_search']); ?>
     </div> -->
 
-	<?php if($page['navigation']): ?>
-		<nav id="main-menu" role="navigation" tabindex="-1" class="medium-8 small-12 columns">
-			<?php print render($page['navigation']); ?>
-		</nav>
-	<?php endif; ?>
+		<div class="navigation medium-8 columns" id="nav">
+			<?php if($page['navigation']): ?>
+				<a href="#nav" class="menu-open"><span>≡</span> Menu</a>
+				<nav id="main-menu" role="navigation" tabindex="-1" class="medium-8 small-12 columns">
+					<?php print render($page['navigation']); ?>
+				</nav>
+				<a href="#top" class="menu-close"><span>x</span> close</a>
+			<?php endif; ?>
+		</div>
+
 
     <?php if($page['header']): ?>
-      <div class="header-region large-12 columns">
+      <div class="header-region medium-12 columns">
         <?php print render($page['header']); ?>
       </div>
     <?php endif; ?>
